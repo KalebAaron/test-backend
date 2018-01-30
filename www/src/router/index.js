a87539cb09549872cb53af8502a4ae47b650081c
Darryl Kilzer <darrylkilzer@gmail.com>
2018-01-30 09:11:23 -0700

    Setting up registration and login. Game is default route currently

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Game from '@/components/Game'
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/game',
       name: 'Game',
       component: Game
     }
